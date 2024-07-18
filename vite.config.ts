94481e52a98e7b6bef2b174a86058ba7497eab0f
韩旭gogogo <js_bin@163.com>
2024-07-18 20:18:58 +0800

    chore: build config update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const base = process.env.NODE_ENV === 'production' ? '/timer-app/' : '/'
 // https://vitejs.dev/config/
 export default defineConfig({
+  base,
   plugins: [react()],
 })
